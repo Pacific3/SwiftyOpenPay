1fe93c879979ff176ad84e6e0560291b0c45b49c
Oscar Swanros <oscar@swanros.com>
2016-01-12 17:09:26 -0600

    Initial commit

---
+++ SwiftyOpenPay/SwiftyOpenPay.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyOpenPay.h
+//  SwiftyOpenPay
+//
+//  Created by Swanros on 1/12/16.
+//  Copyright © 2016 Pacific3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyOpenPay.
+FOUNDATION_EXPORT double SwiftyOpenPayVersionNumber;
+
+//! Project version string for SwiftyOpenPay.
+FOUNDATION_EXPORT const unsigned char SwiftyOpenPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyOpenPay/PublicHeader.h>
+
+
