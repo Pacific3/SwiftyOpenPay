8542ae1e626d7b053c744cbaaa32f696ff58e025
Oscar Swanros <oscar@swanros.com>
2016-01-13 12:13:11 -0600

    Add dependencies, prepare for Carthage

--- SwiftyOpenPay/SwiftyOpenPay.h
+++ SwiftyOpenPay/SwiftyOpenPay.h
@@ -2,18 +2,17 @@
 //  SwiftyOpenPay.h
 //  SwiftyOpenPay
 //
-//  Created by Swanros on 1/12/16.
+//  Created by Oscar Swanros on 1/12/16.
 //  Copyright © 2016 Pacific3. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <PNetworkKit/PNetworkKit.h>
+#import <PUIKit/PUIKit.h>
+#import <PFoundation/PFoundation.h>
 
 //! Project version number for SwiftyOpenPay.
 FOUNDATION_EXPORT double SwiftyOpenPayVersionNumber;
 
 //! Project version string for SwiftyOpenPay.
 FOUNDATION_EXPORT const unsigned char SwiftyOpenPayVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SwiftyOpenPay/PublicHeader.h>
-
-
